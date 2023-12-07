5578838b7d746e103d726eb0ffe08b4dfaad4fa8
ibrahem hussaein <105463095+IbrahemHussein@users.noreply.github.com>
2023-12-07 17:33:45 +0200

    Add files via upload

---
+++ Student Performance Analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project centers around the collection, analysis, and interpretation of student performance data obtained through web scraping. By extracting marks or grades from an educational platform or website, this project aims to uncover patterns, trends, and insights within the academic performance of students.',
+    author='ibrahem',
+    license='',
+)
